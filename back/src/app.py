8cde5f3a5ec10b65ad493990ef14c894440dfce9
Matt Kramer <matt314159@gmail.com>
2018-09-09 14:58:37 -0700

    Fix gevent.monkey

--- back/src/app.py
+++ back/src/app.py
@@ -7,8 +7,8 @@
 # From inspecting gunicorn's ggevent.py, we see that they use subprocess=True,
 # so do the same here. In theory, as long as we create our Flask before doing
 # anything else, this shouldn't be necessary. We can try removing it later.
-import gevent
-gevent.monkey.patch_all(subprocess=True)
+from gevent import monkey
+monkey.patch_all(subprocess=True)
 
 from flask import Flask, jsonify, request
 
