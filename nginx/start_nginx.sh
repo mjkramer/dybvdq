33f286a0f34453a33e90fe1f910dcbd5c7259dc0
Matt Kramer <matt314159@gmail.com>
2018-09-09 00:28:30 -0700

    Fix envsubst invocation

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst < /nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$NGINX_HOSTNAME' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "${NGINX_AUTH_USER}:${NGINX_AUTH_PASS_HASH}" > /htpasswd
 
