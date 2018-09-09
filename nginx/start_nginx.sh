a19c5c72c404b5bdc44cf233afa96c235215e3c8
Matt Kramer <matt314159@gmail.com>
2018-09-09 00:33:02 -0700

    Missing semicolon in nginx command

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,4 +4,4 @@ envsubst '$NGINX_HOSTNAME' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "${NGINX_AUTH_USER}:${NGINX_AUTH_PASS_HASH}" > /htpasswd
 
-exec nginx -g 'daemon off'
+exec nginx -g 'daemon off;'
