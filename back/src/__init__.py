131a5ab1474d943c90a183d643ca722146de339b
Matt Kramer <matt314159@gmail.com>
2018-09-10 09:59:21 -0700

    Check for IPython before gevent monkey patching

--- back/src/__init__.py
+++ back/src/__init__.py
@@ -1,7 +1,20 @@
 "DYBVDQ backend"
 
+# We apply gevent's monkey patching as early as possible, just to be safe.
+# From inspecting gunicorn's ggevent.py, we see that they use subprocess=True,
+# so do the same here. In theory, as long as we create our Flask before doing
+# anything else, this shouldn't be necessary. We can try removing it later.
+# Since IPython doesn't like this, we check for __IPYTHON__.
+
+try:
+    __IPYTHON__
+except NameError:
+    from gevent import monkey
+    monkey.patch_all(subprocess=True)
+
 from flask import Flask
 
 app = Flask('dybvdq')           # pylint: disable=invalid-name
 
+# This will begin the actual app setup
 from . import routes            # pylint: disable=unused-import,wrong-import-position
--- back/src/routes.py
+++ back/src/routes.py
@@ -3,14 +3,6 @@
 
 # pylint: disable=wrong-import-order,wrong-import-position
 
-# We apply gevent's monkey patching as early as possible, just to be safe.
-# From inspecting gunicorn's ggevent.py, we see that they use subprocess=True,
-# so do the same here. In theory, as long as we create our Flask before doing
-# anything else, this shouldn't be necessary. We can try removing it later.
-# Note: If an interactive "import app" hangs, comment out these lines.
-# from gevent import monkey
-# monkey.patch_all(subprocess=True)
-
 from flask import jsonify, request
 from sqlalchemy.dialects import mysql
 
