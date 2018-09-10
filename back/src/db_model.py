ee50167ba8e015113147d120dd4957117a0c4df8
Matt Kramer <matt314159@gmail.com>
2018-09-09 19:56:08 -0700

    Aaaand add db_model.py

---
+++ back/src/db_model.py
@@ -0,0 +1,15 @@
+"Local database for DYB Visual DQ"
+
+# pylint: disable=missing-docstring,too-few-public-methods,no-member
+
+from db import db
+
+class Tagging(db.Model):
+    __bind_key__ = 'app_db'
+
+    session = db.Column(db.String, primary_key=True)
+    fileno = db.Column(db.Integer, primary_key=True, nullable=False)
+    runno = db.Column(db.Integer, primary_key=True, nullable=False)
+    hall = db.Column(db.Integer, primary_key=True, nullable=False)
+
+db.create_all()
