5ed83c48c7ce8b3c108c6f945b754a7e5f4736a1
Matt Kramer <matt314159@gmail.com>
2018-09-11 13:10:23 -0700

    Clean up HallSelector

--- front/src/components/HallSelector.tsx
+++ front/src/components/HallSelector.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux';
-import { Dispatch } from 'redux';
 
 import { setHall } from '../actions';
 import { AppState } from '../model';
-import { Omit } from '../util';
 import DynamicDropdown, { Props as DDProps } from './DynamicDropdown';
 
-type Props = Omit<DDProps, 'items'>;
+type StateProps = Pick<DDProps, 'currentItem'>;
+type DispatchProps = Pick<DDProps, 'onSelect'>;
+type Props = StateProps & DispatchProps;
 
-const HallSelectorView: React.SFC<Props> = ddProps => (
-  <DynamicDropdown items={['EH1', 'EH2', 'EH3']} {...ddProps} />
+const HallSelectorView: React.SFC<Props> = ({ currentItem, onSelect }) => (
+  <DynamicDropdown
+    currentItem={currentItem}
+    items={['EH1', 'EH2', 'EH3']}
+    onSelect={onSelect}
+  />
 );
-
-type StateProps = Pick<Props, 'currentItem'>;
 
 const mapStateToProps: MapStateToProps<StateProps, {}, AppState> = ({ hall }) => ({
   currentItem: hall,
 });
 
-type DispatchProps = Pick<Props, 'onSelect'>;
-
-// XXX Can we use the object form of mapDispatchToProps?
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (
-  dispatch: Dispatch,
-) => ({
-  onSelect: newHall => dispatch(setHall(newHall)),
-});
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = {
+  onSelect: setHall,
+};
 
 export default connect(
   mapStateToProps,
