b4e8eefa703b244bb02282a078a31aae40706ab8
Matt Kramer <matt314159@gmail.com>
2018-09-13 14:50:18 -0700

    Add insertdate to app_db.tagging

--- back/src/model.py
+++ back/src/model.py
@@ -12,9 +12,10 @@ class Tagging(db.Model):
     __bind_key__ = 'app_db'
 
     session = db.Column(db.String(80), primary_key=True)
-    fileno = db.Column(db.Integer, primary_key=True, nullable=False)
-    runno = db.Column(db.Integer, primary_key=True, nullable=False)
-    hall = db.Column(db.Integer, primary_key=True, nullable=False)
+    timestamp = db.Column(db.DateTime, primary_key=True)
+    hall = db.Column(db.Integer, primary_key=True)
+    runno = db.Column(db.Integer, primary_key=True)
+    fileno = db.Column(db.Integer, primary_key=True)
 
 class DqDetectorNew(db.Model):
     __tablename__ = 'DqDetectorNew'
