162ed0db49d813a0e623f44c8eb2379748b07645
Matt Kramer <matt314159@gmail.com>
2018-09-13 15:26:04 -0700

    Add timestamp

--- back/src/model.py
+++ back/src/model.py
@@ -3,6 +3,8 @@
 # pylint: disable=missing-docstring,too-few-public-methods,no-member
 
 from mypy_extensions import TypedDict
+from sqlalchemy.sql.expression import func
+
 from .db import db
 
 DataLocation = TypedDict('DataLocation',  # pylint: disable=invalid-name
@@ -12,10 +14,10 @@ class Tagging(db.Model):
     __bind_key__ = 'app_db'
 
     session = db.Column(db.String(80), primary_key=True)
-    timestamp = db.Column(db.DateTime, primary_key=True)
     hall = db.Column(db.Integer, primary_key=True)
     runno = db.Column(db.Integer, primary_key=True)
     fileno = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, default=func.now())
 
 class DqDetectorNew(db.Model):
     __tablename__ = 'DqDetectorNew'
