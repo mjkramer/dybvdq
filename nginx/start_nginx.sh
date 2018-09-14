bfa9e0b4d2dc32b362af71b7fa07f844b2c35053
Matt Kramer <matt314159@gmail.com>
2018-09-13 22:12:06 -0700

    More friggin sleeping

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,6 +4,6 @@ envsubst '$DYBVDQ_HOSTNAME' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "${DYBVDQ_AUTH_USER}:${DYBVDQ_AUTH_PASS_HASH}" > /htpasswd
 
-sleep 5                         # wait for backend (our upstream)
+sleep 20                         # wait for backend (our upstream)
 
 exec nginx -g 'daemon off;'
