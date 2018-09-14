43dc8c9c2d2235905c2c1ef59de520213b7646ab
Matt Kramer <matt314159@gmail.com>
2018-09-13 22:10:19 -0700

    Fix sleeping for nginx

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,4 +4,6 @@ envsubst '$DYBVDQ_HOSTNAME' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "${DYBVDQ_AUTH_USER}:${DYBVDQ_AUTH_PASS_HASH}" > /htpasswd
 
+sleep 5                         # wait for backend (our upstream)
+
 exec nginx -g 'daemon off;'
