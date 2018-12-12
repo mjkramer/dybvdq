a274f3c75165047441f3d5b399a4303660dc8075
Matt Kramer <matt314159@gmail.com>
2018-12-12 12:51:45 -0800

    Make start_nginx.sh use wait_backend.sh

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -4,6 +4,7 @@ envsubst '$DYBVDQ_HOSTNAME:$DYBVDQ_SUSPEND_CODE' < /nginx.conf.template > /etc/n
 
 echo "${DYBVDQ_AUTH_USER}:${DYBVDQ_AUTH_PASS_HASH}" > /htpasswd
 
-sleep 20                         # wait for backend (our upstream)
+# sleep 20                         # wait for backend (our upstream)
+/wait_backend.sh
 
 exec nginx -g 'daemon off;'
